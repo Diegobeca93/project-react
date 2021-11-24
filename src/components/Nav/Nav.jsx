2f437a4da1ceeec20dc75799a6047d5405621ba4
Diego Becacecci <dbecacecci@gmail.com>
2021-11-24 12:57:51 -0300

    completado y estilos de Navbar #3

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -1,20 +1,84 @@
 import React from 'react'
-import './style.css'
+// import './style.css'
+import styled from 'styled-components'
+import SearchIcon from '@material-ui/icons/Search';
+import Badge from '@material-ui/core/Badge';
+import ShoppingCartOutlinedIcon from '@material-ui/icons/ShoppingCartOutlined';
+
+const Container = styled.div`
+    height: 60px;
+    
+`;
+const Wrapper = styled.div`
+    padding: 10px 20px;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+`;
+const Left = styled.div`
+    flex: 1;
+    display:flex;
+    align-items: center;
+`;
+const SearchContainer = styled.div`
+    border: 0.5px solid  gray;
+    display:flex;
+    align-items: center;
+    margin-left: 25px;
+    padding: 5px;
+    font-family: 'Montserrat', sans-serif;
+`
+const Input = styled.input`
+    border: none;
+`
+const Center = styled.div`
+    flex: 1;
+    text-align: center;
+`;
+const Logo = styled.h1`
+    font-family: 'Montserrat', sans-serif;
+    font-weight: 700;
+`
+
+const Right = styled.div`
+    flex: 1;
+    display: flex;
+    align-items: center;
+    justify-content: flex-end;
+`
+const MenuItems = styled.div`
+    margin-left: 25px;
+`
 
 const Nav = () => {
     return (
-        <div className="navContainer">
-            <div className="logoNav">
-                <h2>My tienda online</h2>
-            </div>
-            <div className="navLinks">
-                <ul>
-                    <li>Celulares</li>
-                    <li>Notebook</li>
-                    <li>Accesorios</li>
-                </ul>
-            </div>
-        </div>
+        <Container>
+            <Wrapper>
+                <Left>
+                    <SearchContainer>
+                        <Input />
+                        <SearchIcon style={ {color: "gray", fontsize: 16} }/>
+                    </SearchContainer>
+                </Left>
+                <Center><Logo>Ecommer</Logo></Center>
+                <Right>
+                    <MenuItems>
+                        REGISTRARSE
+                    </MenuItems>
+                    <MenuItems>
+                        SING IN
+                    </MenuItems>
+                    <MenuItems>
+                        <Badge 
+                        badgeContent={4} 
+                        color="primary">
+                            <ShoppingCartOutlinedIcon />
+                        </Badge>
+                    </MenuItems>
+                </Right>
+            </Wrapper>
+        </Container>
+
     )
 }
 
